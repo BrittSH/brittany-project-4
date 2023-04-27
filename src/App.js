6d9c562031991389af2dffd57e01b3158a54669c
BrittSH <89482952+BrittSH@users.noreply.github.com>
2023-04-27 11:56:00 -0500

    modified src/App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Brittany S Project 4<code>src/App.js</code> and save to reload.
+          Brittany S Project 4
         </p>
         <a
           className="App-link"
